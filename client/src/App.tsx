e41fcf10d528b55553969c2d2e219faf9a70e2f3
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-11-19 02:04:49 +0530

    add header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
 import AuthorizedRoute from "./components/authRoute";
 
-import Dashboard from "./pages/homepage";
+import Header from "./components/header";
+
+import Home from "./pages/home";
+import SignIn from "./pages/signin";
 
 const App: React.FC = ({ user }) => {
   return (
     <ChakraProvider resetCSS={true}>
+      <Header />
       <Switch>
-        <AuthorizedRoute exact path="/" user={true} component={Dashboard} />
-        <Route
-          exact
-          path="/auth"
-          render={() => (!user ? <Redirect to="/" /> : "Log in!")}
-        />
+        <AuthorizedRoute exact path="/" user={true} component={Home} />
+        <Route exact path="/auth" component={SignIn} />
       </Switch>
     </ChakraProvider>
   );
