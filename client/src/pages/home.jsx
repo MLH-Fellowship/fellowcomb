dc9cca73708bceacb49fd828209ce4442bd811f6
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-11-19 16:18:40 +0530

    add wrapper for clusters

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Flex, Heading } from "@chakra-ui/react";
 
 import Profile from "../components/profile";
-import Cluster from "../components/cluster";
+import ClusterWrapper from "../components/clusterWrapper";
 
 const Home = () => {
+  const color = "red";
   return (
     <Flex
       width="90vw"
@@ -21,25 +22,9 @@ const Home = () => {
           github: "https://github.com/jcs98",
           discord: "https://discordapp.com/users/206875427631923200",
         }}
-        color="orange"
+        color={color}
       />
-      <Flex mt="5vh" mb="1.8vh" px="2vw" width="full">
-        <Heading width="full" size="lg">
-          Clusters
-        </Heading>
-      </Flex>
-      <Cluster
-        data={{
-          name: "Pod 1.0.3",
-          users: ["jcs98", "flozender", "utkarsh867"],
-          defaultCluster: true,
-        }}
-        color="orange"
-      />
-      <Cluster
-        data={{ name: "Jest", users: ["jcs98", "flozender"] }}
-        color="red"
-      />
+      <ClusterWrapper username="jcs98" color={color} />
     </Flex>
   );
 };
