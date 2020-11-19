0ade14abbf46556426aedcb528bace3e49d603e6
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-11-19 11:28:32 +0530

    slap together components

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -11,8 +11,6 @@ import {
 } from "@chakra-ui/react";
 import Honeycomb from "../assets/Honeycomb.png";
 
-import Profile from "../components/profile";
-
 const Dashboard = () => {
   const { REACT_APP_GITHUB_CLIENT_ID } = process.env;
 
@@ -22,18 +20,7 @@ const Dashboard = () => {
       align="center"
       justifyContent="center"
       direction="column"
-      height="60vh"
     >
-      {/* <Profile
-        user={{
-          name: "Jainam Shah",
-          calendly: "",
-          linkedin: "https://linkedin.com/in/jainam-chirag-shah",
-          github: "https://github.com/jcs98",
-          discord: "https://discordapp.com/users/206875427631923200",
-        }}
-        color="teal"
-      /> */}
       <Image width="7rem" src={Honeycomb} />
       <Box>
         <Heading my={"2rem"} fontFamily="'KoHo', sans-serif;">
