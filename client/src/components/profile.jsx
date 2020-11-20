456a29f89cf67ff5893c90d30087a0040f54ca30
Jainam Chirag Shah <jcshah98@gmail.com>
2020-11-20 09:51:55 +0530

    Create input for socials (#39)

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Flex,
@@ -10,13 +10,42 @@ import {
   HStack,
   Divider,
   Heading,
+  useDisclosure,
+  Button,
+  Stack,
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
 } from "@chakra-ui/react";
 import { FaCalendarAlt, FaLinkedin, FaGithub, FaDiscord } from "react-icons/fa";
 
 const Profile = ({ user, color }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [socialType, setSocialType] = useState("");
+  const [socialValue, setSocialValue] = useState("");
+
   const iconColor = `${color}.500`;
   const iconHoverColor = `${color}.400`;
   const disabledColor = "gray.400";
+
+  const addOrOpen = (type, currentUrl) => {
+    if (currentUrl) {
+      window.location.href = currentUrl;
+    } else {
+      setSocialType(type);
+      onOpen();
+    }
+  };
+
+  const updateDetails = (type, url) => {
+    // TODO api request to update url
+    console.log(socialType + ":" + socialValue);
+    onClose();
+  };
 
   return (
     <>
@@ -26,37 +55,64 @@ const Profile = ({ user, color }) => {
           {user.name}
         </Heading>
         <Spacer />
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Update your details</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Stack spacing="4">
+                <Text>{socialType} url</Text>
+                <Input
+                  label="url"
+                  onChange={(e) => setSocialValue(e.target.value)}
+                />
+                <Button colorScheme="yellow" onClick={updateDetails}>
+                  Save
+                </Button>
+              </Stack>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
         <HStack spacing="4">
-          <Link href={user.calendly} isExternal>
+          <Link onClick={() => addOrOpen("Calendly", user.calendly)}>
             <Icon
               as={FaCalendarAlt}
               boxSize="6"
               color={user.calendly ? iconColor : disabledColor}
-              _hover={{ color: user.calendly ? iconHoverColor : disabledColor }}
+              _hover={{
+                color: user.calendly ? iconHoverColor : disabledColor,
+              }}
             />
           </Link>
-          <Link href={user.linkedin} isExternal>
+          <Link onClick={() => addOrOpen("LinkedIn", user.linkedin)}>
             <Icon
               as={FaLinkedin}
               boxSize="6"
               color={user.linkedin ? iconColor : disabledColor}
-              _hover={{ color: user.linkedin ? iconHoverColor : disabledColor }}
+              _hover={{
+                color: user.linkedin ? iconHoverColor : disabledColor,
+              }}
             />
           </Link>
-          <Link href={user.github} isExternal>
+          <Link onClick={() => addOrOpen("GitHub", user.github)}>
             <Icon
               as={FaGithub}
               boxSize="6"
               color={user.github ? iconColor : disabledColor}
-              _hover={{ color: user.github ? iconHoverColor : disabledColor }}
+              _hover={{
+                color: user.github ? iconHoverColor : disabledColor,
+              }}
             />
           </Link>
-          <Link href={user.discord} isExternal>
+          <Link onClick={() => addOrOpen("Discord", user.discord)}>
             <Icon
               as={FaDiscord}
               boxSize="6"
               color={user.discord ? iconColor : disabledColor}
-              _hover={{ color: user.discord ? iconHoverColor : disabledColor }}
+              _hover={{
+                color: user.discord ? iconHoverColor : disabledColor,
+              }}
             />
           </Link>
         </HStack>
