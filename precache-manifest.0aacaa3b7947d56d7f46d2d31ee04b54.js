dae9274066b455c1f1dd8367a71c7abb9173f7b9
--replace-all <utkarsh867@gmail.com>
2021-02-03 03:03:15 +0800

    Updates

---
+++ precache-manifest.0aacaa3b7947d56d7f46d2d31ee04b54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3fecaf9339cd790497fb767475e797",
+    "url": "/index.html"
+  },
+  {
+    "revision": "014d7242a2a45a0be7b1",
+    "url": "/static/js/2.335a8fcc.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.335a8fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e6e57e6be6984762c6",
+    "url": "/static/js/main.3000269a.chunk.js"
+  },
+  {
+    "revision": "505ce02e76a40622dca8",
+    "url": "/static/js/runtime-main.0ee4a979.js"
+  }
+]);
