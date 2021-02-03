424d5674ac05c777c5e67a25b26108a205a65e95
--replace-all <utkarsh867@gmail.com>
2021-02-03 23:21:36 +0800

    Fix server url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
 export const BACKEND_SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? "https://fellowcomb-api.herokuapp.com/"
+    ? "https://fellowcomb-api.herokuapp.com"
     : "http://localhost:4000";
 export const CLIENT_URL =
   process.env.NODE_ENV === "production"
